ebbbdea5a13e88e7a4eaa209a7eacefa7af3eb1d
sameem786-blip <sameemabbas2002@outlook.com>
2021-10-04 15:21:59 +0500

    contact Us Section designed

--- src/components/ContactUs/contactus.js
+++ src/components/ContactUs/contactus.js
@@ -1,31 +1,38 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from "reactstrap";
+import SectionTitle from "../../components/Common/SectionTitle";
 
 class ContactUs extends Component {
   render() {
+    const submitForm = () => {
+      console.log('Form Submitted');
+    }
     return (
       <React.Fragment>
-        {/* CTA START  */}
         <section className="section position-relative bg-gradient">
           <div className="bg-cta-overlay" />
-          <Container className="container">
-            <Row className="justify-content-center">
-              <Col lg={6}>
-                <div className="text-center">
-                  <h2 className="text-white mb-3">
-                    Do You Need Success Product Also ?
-                  </h2>
-                  <Link to="#" className="btn btn-light mt-4">
-                    Estimate Project
-                  </Link>
-                  <Link to="#" className="text-white-50">
-                    <p className="mb-0 mt-4">Or contact with us</p>
-                  </Link>
-                </div>
+          <SectionTitle
+              title="Request A Quote"
+              description="Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit sed consequuntur as sequi nesciunt."
+            />
+          <div className="form-container">
+          <form onSubmit={this.submitForm}>
+            <div className="row">
+              
+              <Col xs="12" lg="6">
+              <input type="text" className="input" placeholder="Name*" required></input>
+              <input type="text" className="input" placeholder="Organization*" required></input>
+              <input type="tel" className="input" placeholder="Contact*" required></input>
+              <input type="email" className="input" placeholder="Email*" required></input>
               </Col>
-            </Row>
-          </Container>
+              <Col xs="12" lg='6'>
+              <textarea className="input message" id="exampleFormControlTextarea1" placeholder="Message" rows="3"></textarea>
+              </Col>
+            </div>
+            <input type="submit" className="form-submit" value="Submit" />
+            </form>
+          </div>
         </section>
 
         {/* CTA END  */}
